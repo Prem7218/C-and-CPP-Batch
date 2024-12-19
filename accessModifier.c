a72572af2ae82a4eb45d112a07aa1fa4d770da17
Prem7218 <premradheshyammahajan@gmail.com>
2024-12-19 15:39:24 +0530

    Restriction's to write variable's....

---
+++ accessModifier.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int number = 10;
+    // <----------------------- Single Line Comment this is.
+    /* <----------------------- Multiple Comment this is.
+        %d ----> integer
+        %c ----> character
+        %s ----> string
+        %ld----> long
+        %lld---> long-long-int
+        %llf---> long-double
+        %p ----> pointers
+        %lf----> double
+        %f ----> float
+        more...
+    */
+
+    printf("The Number: %d", number);
+
+    return 0;
+}
